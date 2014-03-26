d44b849995d15425fb931a0a728d82438da09536
Lim Mingjie Kenneth <kenlimmj@gmail.com>
2014-03-26 14:35:09 -0400

    Implemented the swipe left and right gestures

--- app/js/gestures.js
+++ app/js/gestures.js
@@ -6,6 +6,13 @@ var zoomState = false;
 // True if text is highlighted, and false otherwise
 var highlightState = false;
 
+// Initialize a boolean to keep track of the collapse state of the side block
+// True if the block is collapsed, and false otherwise
+var collapseState = false;
+
+// Get the DOM Node for the main block
+container = document.querySelector("main");
+
 ////////////////////////////////////////////////////////////////////////////////
 
 // An instance calls a pull gesture on the canvas
@@ -16,9 +23,6 @@ var highlightState = false;
 function pull(arr) {
     // Get the DOM Node below the current cursor location
     var elem = document.elementFromPoint(arr[0], arr[1]);
-
-    // Get the DOM Node for the main block
-    container = document.querySelector("main");
 
     if (jQuery.contains(container, elem)) {
         // Only do something if we're not currently zoomed in
@@ -55,25 +59,53 @@ function push(arr) {
 }
 
 // An instance calls a swipe left gesture on the canvas
-// A swipe left gesture is a positive change in the x-axis made with a closed hand
+// A swipe left gesture is a negative change in the x-axis made with a closed hand
 // - Swiping left on the main block advances to the next item if the block is currently
 //   in a zoomed state. If the block is not zoomed, swiping does nothing.
 // - Swiping left on the side block collapses the side block if the block is currently
 //   expanded. If the block is not expanded, swiping does nothing.
 // Input: Array of float x and float y coordinates of the mapped screen coordinates
 function swipeleft(arr) {
+    // Get the DOM Node below the current cursor location
+    var elem = document.elementFromPoint(arr[0], arr[1]);
 
+    if (jQuery.contains(container, elem)) {
+        if (zoomState === true) {
+            // Initialize a handler to the "Previous" button
+            var elem = document.getElementById("zoomNext");
+
+            // Mimic a click on the button
+            // FIXME: This is a very hacky method for interfacing with the Zoomoz API
+            elem.click();
+        }
+    } else {
+        // Expand the side block
+    }
 }
 
 // An instance calls a swipe right gesture on the canvas
-// A swipe right gesture is a negative change in the x-axis made with a closed hand
+// A swipe right gesture is a positive change in the x-axis made with a closed hand
 // - Swiping right on the main block backtracks to the previous item if the block is currently
 //   in a zoomed state. If the block is not zoomed, swiping does nothing.
 // - Swiping right on the side block expands the side block if the block is currently
 //   collapsed. If the block is not collapsed, swiping does nothing.
 // Input: Array of float x and float y coordinates of the mapped screen coordinates
 function swiperight(arr) {
+    // Get the DOM Node below the current cursor location
+    var elem = document.elementFromPoint(arr[0], arr[1]);
 
+    if (jQuery.contains(container, elem)) {
+        if (zoomState === true) {
+            // Initialize a handler to the "Next" button
+            var elem = document.getElementById("zoomPrev");
+
+            // Mimic a click on the button
+            // FIXME: This is a very hacky method for interfacing with the Zoomoz API
+            elem.click();
+        }
+    } else {
+        // Collapse the side block
+    }
 }
 
 // An instance calls a swipe up gesture on the canvas
