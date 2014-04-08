96a95829c9740ca72b1fef6bfb6e8fdc8f0670a9
Lim Mingjie Kenneth <kenlimmj@gmail.com>
2014-04-07 22:02:03 -0400

    Reduced the number of frames averaged back down to 2 to prevent jankiness :(

--- app/js/websocket.js
+++ app/js/websocket.js
@@ -155,7 +155,7 @@ function createWebSocket() {
 
             // Average the data over a pair of frames for increased accuracy
             // FIXME: Dynamically change the framerate depending on what the user is doing
-            var averagedData = averageFrames(coordData, 25);
+            var averagedData = averageFrames(coordData, 2);
 
             // Calculate the precision threshold value for each hand
             var lthreshold = cursorThreshold(averagedData.lhandState),
